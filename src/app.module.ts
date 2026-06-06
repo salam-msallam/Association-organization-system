c71e56335c4324775d6778b0f5c91d26fd41df8e
salam-msallam <msallamsalma4@gmail.com>
2026-06-06 15:17:47 +0300

    i18n setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path'; 
 
 @Module({
-  imports: [PrismaModule],
+  imports: 
+  [PrismaModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'ar', 
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'), 
+        watch: true,
+      },
+      resolvers: [
+        
+        new AcceptLanguageResolver(), 
+         
+      ],
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { i18nValidationErrorFactory, I18nValidationExceptionFilter } from 'nestjs-i18n'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,13 +10,20 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Charity Management System API')
-    .setDescription('توثيق خدمات نظام إدارة الجمعية الخيرية (منصة كريم)')
+    .setDescription('توثيق خدمات نظام إدارة الجمعية الخيرية')
     .setVersion('1.0')
     .addBearerAuth() 
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); 
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: i18nValidationErrorFactory,
+    }),
+  );
+  app.useGlobalFilters(new I18nValidationExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
