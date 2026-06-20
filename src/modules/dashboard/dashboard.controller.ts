e943a8dd1fe318cb2673d53a18a89b07b1aec51d
Hala-Erksousi <halaerksousi@gmail.com>
2026-06-20 12:39:53 +0300

    fix authorized dashboard

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import {ApiOperation, ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 @Controller('admin/dashboard')
-@UseGuards(JwtAuthGuard)
+@UseGuards(AuthGuard('jwt')) 
 @ApiTags('Dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
-
-  @ApiBearerAuth()
+  
+  @ApiBearerAuth('jwt')
   @Get('summary')
   async getDashboardStatistics() {
     return await this.dashboardService.getDashboardStats();
